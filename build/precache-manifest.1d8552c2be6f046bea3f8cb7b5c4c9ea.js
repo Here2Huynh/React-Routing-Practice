7c036d95aa96630c9baa795a27bf9c39be403847
John Huynh <jhnhnyh9@gmail.com>
2018-10-02 16:26:13 -0700

    first commit

---
+++ build/precache-manifest.1d8552c2be6f046bea3f8cb7b5c4c9ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4620f595c546affbc3c3",
+    "url": "/static/js/main.4620f595.chunk.js"
+  },
+  {
+    "revision": "36d218cf41f90aa22e99",
+    "url": "/static/js/1.36d218cf.chunk.js"
+  },
+  {
+    "revision": "4620f595c546affbc3c3",
+    "url": "/static/css/main.50885726.chunk.css"
+  },
+  {
+    "revision": "fdd4c22be631fe9e6d7c18e376bb74f8",
+    "url": "/index.html"
+  }
+];
---
+++ src/components/noMatch.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const noMatch = () => (
+    <h1 style={{ textAlign: 'center' }}> 404 Things were not found </h1>
+)
+
+export default noMatch;
